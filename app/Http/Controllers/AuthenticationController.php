50bef8b0af400afdb674509fd5e890833a86e6eb
rizky14045 <mrizkysaputra17@gmail.com>
2025-09-11 15:08:08 +0700

    add factory vaultsite

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -10,7 +10,8 @@ class AuthenticationController extends Controller
 {
 
     public function __construct(
-        protected UserService $userService
+        protected UserService $userService,
+        protected VaultSite $vaultSite
     ) {}
     
     public function viewLogin(): View {
@@ -18,5 +19,50 @@ class AuthenticationController extends Controller
     }
     public function viewRegister(): View {
         return view('register');
+    }
+
+    public function addData(){
+         $data = [
+            "CardNo" => "20211114",
+            "Name" => "Test API 211114",
+            "CardPinNo" => "0000",
+            "CardType" => "Normal",
+            "Department" => "RnD",
+            "Company" => "binRusdi",
+            "Gentle" => "Male",
+            "AccessLevel" => "02",
+            "FaceAccessLevel" => "00",
+            "LiftAccessLevel" => "01",
+            "BypassAP" => false,
+            "ActiveStatus" => true,
+            "NonExpired" => true,
+            "ExpiredDate" => "2020/12/31",
+            "VehicleNo" => "B6694UWA",
+            "FloorNo" => "3",
+            "UnitNo" => "25",
+            "ParkingNo" => "4",
+            "StaffNo" => "12514",
+            "Title" => "string",
+            "Position" => "string",
+            "NRIC" => "string",
+            "Passport" => "string",
+            "Race" => "string",
+            "DOB" => "1985/07/10",
+            "JoiningDate" => "2020/01/01",
+            "ResignDate" => "2020/12/31",
+            "Address1" => "string",
+            "Address2" => "string",
+            "PostalCode" => "string",
+            "City" => "string",
+            "State" => "string",
+            "Email" => "string",
+            "MobileNo" => "string",
+            "Photo" => "string",
+            "DownloadCard" => true,
+        ];
+
+        $response = $soap->addCard($data);
+
+        return response()->json($response);
     }
 }
