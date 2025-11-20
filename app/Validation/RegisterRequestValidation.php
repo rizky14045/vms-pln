00736b1b8b7189a99d4e43d84058f40aad341486
alifnrz27 <alifnaufalrizki27@gmail.com>
2025-11-20 12:57:48 +0700

    edit add card format

--- app/Validation/RegisterRequestValidation.php
+++ app/Validation/RegisterRequestValidation.php
@@ -9,12 +9,11 @@ class RegisterRequestValidation
         return [
             'nid' => 'required|string|max:255',
             'name' => 'required|string|max:255',
-            'person_image' => 'required|file|mimes:png,jpeg,jpg', 
+            'person_image' => 'required|file|mimes:png,jpeg,jpg|dimensions:max_width=1920,max_height=1080',
             'email' => 'required|email:rfc,dns',
             'phone' => 'required|regex:/^628\d{7,12}$/',
         ];
     }
-
 
     public static function messages()
     {
@@ -29,13 +28,15 @@ class RegisterRequestValidation
 
             'person_image.required' => 'Foto wajib diisi.',
             'person_image.file' => 'Foto harus berupa file.',
-            'person_image.mimes' => 'Format foto tidak sesuai.',
+            'person_image.mimes' => 'Format foto tidak sesuai. Hanya PNG, JPEG, dan JPG yang diizinkan.',
+            'person_image.dimensions' => 'Resolusi foto tidak boleh lebih dari 1920x1080 piksel.',
 
             'email.required' => 'Email wajib diisi.',
             'email.email' => 'Format email tidak sesuai.',
 
             'phone.required' => 'Nomor telepon wajib diisi.',
-            'phone.regex' => 'Format nomor telepon tidak sesuai. Contoh: 628'
+            'phone.regex' => 'Format nomor telepon tidak sesuai. Contoh: 628XXXXXXXX.',
         ];
     }
+
 }
